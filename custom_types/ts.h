6a35500945cedc8d829e56049c3066376e117e89
burmyakov <artem.burmyakov@gmail.com>
2021-12-29 00:59:45 +0300

    Create ts.h

---
+++ custom_types/ts.h
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+
+struct TS {
+	// Task system
+	int n; // n. of tasks -- must be less than MAXN
+	static const int MAXN = 16; 
+	int C[MAXN]; // WCETs
+	int D[MAXN]; // deadlines
+	int P[MAXN]; // minimum interarrival times
+	
+	int pmax; // max(P_i) (buffered value)
+	TS() { }
+    
+	TS(int n_) : n(n_), pmax(0) { assert(n <= MAXN); }
+    
+	void setTask(int i, int Ci, int Di, int Pi) {
+		C[i] = Ci;
+		D[i] = Di;
+		P[i] = Pi;
+        
+		pmax = std::max(pmax, Pi);
+        
+        if (D[i] > P[i]) {
+            std::cout << "Problem: " << D[i] << ", " << P[i] << std::endl;
+        }
+		assert(C[i] <= D[i]); 
+		assert(D[i] <= P[i]); // * constrained deadlines *
+	}
+    
+	int Pmax() const { return pmax; }
+	void read();
+};
+
